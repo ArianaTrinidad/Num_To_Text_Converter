4f0e73e16a3082dfd8e8142dba346d2bd11c5e07
ArianaTrinidad <quirino.ariana1@gmail.com>
2022-09-14 20:21:27 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Num_To_Text_Converter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tipee el número que quiere psasr a texto");
+
+            var num = Console.ReadLine();
+
+            var partialResult = new Converter();
+
+            var result = partialResult.InText(num);
+
+            Console.WriteLine(result);
+        }
+    }
+}
